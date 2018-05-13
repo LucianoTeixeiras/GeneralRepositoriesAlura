dcfa54936fa5e3991da93aebecf1a22237999261
Luciano Teixeira <luciano.teixeiras@gmail.com>
2018-05-13 20:48:44 -0300

    Commit Cap 02
    Commit Cap 02

---
+++ Cap1Aula05_CriandoDataBase.sql
@@ -0,0 +1,35 @@
+--Criando Usuario para o Curso
+
+CREATE USER cursoplsql IDENTIFIED BY cursoplsql
+DEFAULT tablespace users;
+
+--Concedendo Privilégios
+
+GRANT connect, resource TO cursoplsql;
+
+--Criando Tabelas
+
+CREATE TABLE SegMercado(ID NUMBER(5),Descricao VARCHAR2(100));
+
+--Restringindo Valores na Tabela
+
+ALTER TABLE Segmercado ADD CONSTRAINT SegMercado_id_pk PRIMARY KEY(ID);
+
+--Criando Tabela Cliente
+
+CREATE TABLE Cliente
+( ID NUMBER(5),
+Razao_Social VARCHAR2(100),
+CNPJ VARCHAR2(20),
+SegMercado_id NUMBER(5),
+Data_Inclusao DATE,Faturamento_Previsto NUMBER(10,2),
+Categoria VARCHAR2(20));
+
+--Concedendo Privilégios
+
+ALTER TABLE Cliente ADD CONSTRAINT Cliente_id_pk PRIMARY KEY(ID);
+
+--Criando FK
+
+ALTER TABLE Cliente ADD CONSTRAINT Cliente_SegMercado_fk FOREIGN KEY(SegMercado_id) REFERENCES Segmercado(id);
+
