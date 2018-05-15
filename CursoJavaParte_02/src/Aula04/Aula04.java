4b97a61f04ed156c783923c0838f6a639188dcda
Luciano Teixeira <luciano.teixeiras@gmail.com>
2018-05-15 10:35:30 -0300

    Commit Inicial Curso Java Parte 2

---
+++ CursoJavaParte_02/src/Aula04/Aula04.java
@@ -0,0 +1,10 @@
+package Aula04;
+
+public class Aula04 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ CursoJavaParte_02/src/Aula05/Aula05.java
@@ -0,0 +1,10 @@
+package Aula05;
+
+public class Aula05 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
