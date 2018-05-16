d20fc6e4d00033ba244570e4317fd8d2088f5b49
Luciano Teixeira <luciano.teixeiras@gmail.com>
2018-05-16 15:29:21 -0300

    Commit Cap 04

--- CursoJavaParte_02/src/Conta.java
+++ CursoJavaParte_02/src/Conta.java
@@ -5,5 +5,31 @@ public class Conta {
 	int agencia = 42;
 	int numero;
 	String titular;
+	
+	public void deposita(double valor) {
+		
+		this.saldo += valor;
+		
+	}
+	
+	public boolean saca(double valor) {
+		if(this.saldo >= valor) {
+			this.saldo -= valor;
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	public boolean transfere(double valor, Conta destino ) {
+		if (this.saldo >= valor) {
+			
+			this.saldo -= valor;
+			destino.deposita(valor);
+			return true;
+			
+		}
+			return false;
+	}
 
 }
--- CursoJavaParte_02/src/TesteReferencia.java
+++ CursoJavaParte_02/src/TesteReferencia.java
@@ -6,9 +6,15 @@ public class TesteReferencia {
 		
 		Conta primeiraConta = new Conta();
 		primeiraConta.saldo = 300;
+		
 		System.out.println("O Saldo da primeira: " + primeiraConta.saldo);
 		
 		Conta segundaConta = primeiraConta;
+		System.out.println("O Saldo da primeira: " + segundaConta.saldo);
+		
+		segundaConta.saldo += 100;
+		System.out.println("O Saldo da primeira: " + segundaConta.saldo);
+		System.out.println("O Saldo da primeira: " + primeiraConta.saldo);
 
 	}
 
